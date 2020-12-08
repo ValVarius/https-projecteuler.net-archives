915053769addbf1ff4b4fab832bd97701577ba82
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-08 13:51:39 -0800

    15

--- 15 Lattice paths.js
+++ 15 Lattice paths.js
@@ -3,3 +3,30 @@
 
 // How many such routes are there through a 20×20 grid?
 
+function factorial(x){
+    // validating the input
+    x = parseInt(x, 10);
+    if (isNaN(x)) return 1;
+    
+    // if x below 0, return 1
+    if (x <= 0) return 1;
+    // if x above 170, return infinity
+    if (x > 170) return Infinity;
+    // calculating the factorial
+    var y = 1;
+    for (var i = x; i>0; i--){
+        y *= i;
+    }
+    return y;
+}
+
+// 2x! / x! *x!
+
+let nominator = factorial(40) 
+let denominator = factorial(20) * factorial(20)
+let solution = nominator / denominator
+
+console.log(nominator);
+console.log(denominator);
+console.log(solution);
+
