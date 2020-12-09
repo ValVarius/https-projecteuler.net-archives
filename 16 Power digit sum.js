e89b065bb4c96ac2f728c1ab552d634770c11d9e
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-08 18:04:17 -0800

    16

--- 16 Power digit sum.js
+++ 16 Power digit sum.js
@@ -2,3 +2,15 @@
 
 // What is the sum of the digits of the number 21000?
 
+let num = BigInt( Math.pow(2,1000)).toString()
+// num = num.toString();
+
+let total = 0;
+
+for (let i = 0; i < num.length; i++) {
+    
+    total += parseInt(num.charAt(i));
+}
+
+console.log(num);
+console.log(total);
