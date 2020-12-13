3beb83dd46da4593a0722e90dec1d7e9e6c405bc
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-12 20:21:48 -0800

    19

--- 19 Counting Sundays.js
+++ 19 Counting Sundays.js
@@ -10,10 +10,8 @@
 // A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-// year = 1900
 dayOfWeek = 1
-// dayOfMonth = 1
-// month = 1;
+
 date = {
     month: 1, 
     day: 1,
@@ -30,13 +28,9 @@ const switchDay =(day) => {
 }
 const switchDayMonth = (mon, y, dayNum) => {
 
-    // console.log(mon);
     if(mon === 4 || mon === 6 || mon === 9 || mon === 11){
-        // console.log("here");
-        // console.log(mon);
-
+      
         if(dayNum === 30){
-            // console.log("here");
             mon++;
             dayNum = 1
         }
@@ -44,9 +38,7 @@ const switchDayMonth = (mon, y, dayNum) => {
         dayNum++;
     }
     else if(mon === 2 && (y % 4) === 0 && (y % 100) !== 0  ){
-        // console.log("here");
         if (dayNum === 29){
-            // console.log("here");
 
             mon++
             dayNum = 1
@@ -55,9 +47,7 @@ const switchDayMonth = (mon, y, dayNum) => {
         dayNum++;
     }
     else if(mon === 2 && (y % 400) === 0){
-        // console.log("here");
         if (dayNum === 29){
-            // console.log("here");
 
             mon++
             dayNum = 1
@@ -66,11 +56,7 @@ const switchDayMonth = (mon, y, dayNum) => {
         dayNum++;
     }
     else if(mon === 2){
-        // console.log("here");
         if (dayNum === 28){
-            // console.log("here");
-            // console.log(mon);
-
             mon++
             dayNum = 1
         }
@@ -78,9 +64,7 @@ const switchDayMonth = (mon, y, dayNum) => {
         dayNum++;
     }
     else if (mon === 12){
-        // console.log("here is 12");
         if (dayNum === 31){
-            // console.log("here");
             mon = 1;
             dayNum = 1
             y++;
@@ -89,25 +73,20 @@ const switchDayMonth = (mon, y, dayNum) => {
         dayNum++;
     }
     else{
-        // console.log("here");
         if (dayNum === 31){
-            // console.log("here");
             mon++;
             dayNum = 1
         }
         else{
-            // console.log("here");
             dayNum++;
         }
     }
 
-// console.log(mon);
     date = {
         month: mon, 
         day: dayNum,
         year: y
     }
-    // console.log(date);
 
     return date;
 
@@ -119,8 +98,7 @@ while(date.year <= 2000){
 
     dayOfWeek = switchDay(dayOfWeek);
     date = switchDayMonth(date.month,date.year,date.day)
-    // console.log(dayOfWeek);
-    // console.log(date);
+  
 
     if(date.day === 1 && dayOfWeek === 7 && date.year>1900){
         numberofSundays++;
