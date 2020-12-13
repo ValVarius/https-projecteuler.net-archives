31f837358022aac17f9f2fd66b3a76a65fb67685
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-13 11:07:30 -0800

    20

--- 20 Factorial digit sum.js
+++ 20 Factorial digit sum.js
@@ -4,3 +4,17 @@
 // and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 // Find the sum of the digits in the number 100!
+
+let total = BigInt(100);
+for (let i = 1; i < 100; i++) {
+  total = total * BigInt(i);
+}
+
+total = total.toString()
+
+let sum = 0;
+for (let i = 0; i < total.length; i++) {
+    let addition = parseInt(total.charAt(i))
+    sum+= addition;
+}
+console.log(sum);
