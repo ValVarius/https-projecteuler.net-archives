574ab6eaeb9b9adb8ef9c51060dce32a748ba5b7
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-14 16:39:46 -0800

    24

--- 24 Lexicographic permutations.js
+++ 24 Lexicographic permutations.js
@@ -5,4 +5,26 @@
 
 // 012   021   102   120   201   210
 
-// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 , 9 
+
+var list = [0,1,2,3,4,5,6,7,8,9];
+
+var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+
+console.log(permute(list)[999999]);
