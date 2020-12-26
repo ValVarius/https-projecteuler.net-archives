694a31d998d9f5bfbd2c93ffed864d11251575dc
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-25 17:56:50 -0800

    36

---
+++ 36 Double-base palindromes.js
@@ -0,0 +1,41 @@
+// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+// Find the sum of all numbers, less than one million, 
+// which are palindromic in base 10 and base 2.
+
+// (Please note that the palindromic number, in either base, may not include leading zeros.)
+const isPalindromeOnBoth = (num) => {
+    let str = num.toString()
+    let bi = num.toString(2)
+    let isPali = true;
+
+
+    str.split("").forEach((c,i) => {
+        if(c!== str[str.length - 1 - i]) {
+            isPali = false;
+        }
+    });
+    num.toString(2).split("").forEach((c,i) => {
+        if(c!== bi[bi.length - 1 - i]) {
+            isPali = false;
+        }
+    });
+
+    
+
+    return isPali;
+}
+
+let sum = 0;
+
+for (let i = 1; i < 1000000; i++) {
+    
+    if(isPalindromeOnBoth(i)){
+        sum+= i
+    }
+    
+}
+
+console.log(sum);
+
+
