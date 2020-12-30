21e81f43298de90bf6ffac40787212f461d1d98c
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-12-29 17:02:01 -0800

    37

--- 37 Truncatable primes.js
+++ 37 Truncatable primes.js
@@ -7,6 +7,7 @@
 // truncatable from left to right and right to left.
 
 // NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+
 const isPrime = (n) => {
   if (n === 1) {
     return false;
@@ -21,61 +22,82 @@ const isPrime = (n) => {
     return true;
   }
 };
+const check = (num) => {
+  let n = num.toString();
+//   console.log(n);
+  if (!isPrime(num)) return false;
 
-let begin = [2, 3, 5, 7];
-let follow = [3, 7, 9];
+  for (let i = 0; i < n.length; i++) {
+    if (!isPrime(parseInt(n.charAt(0)))) return false;
+    if (!isPrime(parseInt(n.substring(i)))) return false;
+    if (!isPrime(parseInt(n.substring(0, n.length - i)))) return false;
+  }
+
+//   console.log(`Found ${n}`);
+
+  return true;
+};
+
+let follow = [1, 3, 7, 9];
+let base = [2, 3, 5, 7];
 let truncatables = [];
-let lastBase;
+let total = 0;
 
+while (base.length > 0 && truncatables.length < 11) {
+  for (let i = 0; i < base.length; i++) {
+    for (let y = 0; y < follow.length; y++) {
+      let num = parseInt(`${base[i]}${follow[y]}`);
+      if (isPrime(num)) {
 
-
-
-
-
-
-
-
-const getTrunc = (num, m) =>{
-
-    let current = num;
-    let firttime = true;
-    for (let y = m; y < follow.length; ) {
-        if (isPrime(parseInt(`${current}${follow[y]}`))) {
-          // check for left primeness?
-          console.log(`Good: ${current}${follow[y]}`);
-          if(y===(follow.length-1)){
-            getTrunc(current, y+1);
-            console.log("here");
-          }
-          current = `${current}${follow[y]}`; 
-          truncatables.push(current);
-
-          y = 0;
-        } else {
-            if(y===(follow.length-1)){
-                getTrunc(current, y+1);
-                console.log("here");
-          console.log(`Bad: ${current}${follow[y]}`);
-          y++;
+        if (check(num)) {
+          truncatables.push(num);
+          total += num;
         }
+        base.push(num);
       }
+      if (y === follow.length - 1) {
+        base.splice(i, 1);
+      }
+    }
+  }
 
-}
-
-for (let i = 0; i < begin.length; i++) {
-  
-    // if(!truncatables.lenght){
-    //     lastBase = begin[i]
-    // }
-    // else if()
-    getTrunc(begin[i],0); 
-
-  
-    
-  
 }
 
 console.log(truncatables);
+console.log(total);
 
 
-// 
+
+
+// Brute Force Version
+// let number = 10
+
+// while(truncatables.length < 11){
+//     if ( check(number) ) {
+
+//         truncatables.push(number);
+//         total += number;
+//     }
+//     number++;
+
+// }
+
+// console.log(truncatables);
+// console.log(total);
+
+// creates a base of numbers
+// for (let i = 0; i < begin.length; i++) {
+//   for (let y = 0; y < follow.length; y++) {
+//     let num = parseInt(`${begin[i]}${follow[y]}`);
+//     if (isPrime(num)) {
+//       base.push(num);
+
+//       if (check(num)) {
+//         truncatables.push(num);
+//         total += num;
+//       }
+//     }
+//   }
+// }
+
+
